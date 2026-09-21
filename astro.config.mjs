8943ab7b372749a5c832118d7f5c9f791da58cef
Cristian <matheuscristianrg@gmail.com>
2026-09-21 06:56:42 -0300

    chore: initialize Astro portfolio project

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://nthcristian.github.io",
+  base: "/portfolio",
+
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
